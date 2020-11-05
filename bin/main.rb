4fffa9e731f67562035aa9e94d49a5ccbc3a4970
CornerstoneII <seunmyles@gmail.com>
2020-11-06 00:54:22 +0100

    Setup user interaction logic

--- bin/main.rb
+++ bin/main.rb
@@ -1,2 +1,15 @@
 #!/usr/bin/env ruby
-puts "Hello world!"
+
+# 1. get_user_data
+end
+# 2. Display Welcome message
+
+# 3. Fetch Data for all exchange rates
+
+# 4. Ensure User has internet connection to run program
+
+# 5. Display table data for all exchange rates in other currencies if connection true
+
+# 6. Else end program
+
+# 7. Display currency rate according to user input data
