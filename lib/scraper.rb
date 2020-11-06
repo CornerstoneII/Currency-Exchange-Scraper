8c159ce7cef8722223d3df436714e040224651b1
CornerstoneII <seunmyles@gmail.com>
2020-11-06 16:48:30 +0100

    Update README file

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,39 +3,38 @@ require 'nokogiri'
 require 'byebug'
 
 class ExchangeScraper
-    attr_reader :unparsed_page, :parsed_page, :connection
+    attr_reader :parsed_page
 
-    def initialize(url = 'https://www.iban.com/exchange-rates')
-      @url = url
-      @unparsed_page = nil
-      @parsed_page = nil
-      @connection = false
+    def initialize
+      url = 'https://www.iban.com/exchange-rates'
+      unparsed_page = HTTParty.get(url)
+      @parsed_page ||= Nokogiri::HTML(unparsed_page)
     end
 
-    def try_connecting
-        begin
-          @unparsed_page = HTTParty.get(@url)
-          @parsed_page = Nokogiri::HTML(@unparsed_page)
-          @connection = true
-        rescue StandardError
-          @connection = false
-        end
-          @connection
+    def get_data
+      exchangeList.each do |e|
+        curr_item = {
+            currency: e.css('td')[0].text.split(' ')[0].gsub('\t',''),
+            currency_name: e.css('td')[1].text,
+            exchangeRate: e.css('td')[2].text.to_f,
+        }
+        all_exchange << curr_item
+      end
     end
+
+    private
 
     def exchangeList
-      @parsed_page.css('table.table > tbody > tr')
+      parsed_page.css('table.table > tbody > tr')
     end
 
-    def supply
-        all_exchange = Array.new
-        exchangeList.each do |e|
-            curr_item = {
-                currency: e.css('td')[0].text.split(' ')[0].gsub('\t',''),
-                currency_name: e.css('td')[1].text,
-                exchangeRate: e.css('td')[2].text.to_f,
-            }
-            all_exchange << curr_item
-          end
-    end
+    scraper = ExchangeScraper.new
+    currency = scraper.get_currency
+    currency_name = scraper.get_currency_name
+    exchange_rate = scraper.get_exchange_rate
+
+  (0...exchange_rate.size).each do |index|
+    puts "- - - index: #{index + 1} - - -"
+    puts "Currency: #{currency[index]} | Currency Name: #{currency_name[index]} | Exchange Rate: #{exchange_rate[index]}"
+  end
 end
