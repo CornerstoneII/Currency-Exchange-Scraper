bfa347d64edae323f96677189bf8f796decd3ac0
CornerstoneII <seunmyles@gmail.com>
2020-11-06 02:16:00 +0100

    Add Display currency rate according to user input data main.rb

--- bin/main.rb
+++ bin/main.rb
@@ -1,5 +1,5 @@
 #!/usr/bin/env ruby
-def get_user_data(check_curr_sym)
+def get_user_data(check_curr_key)
     input = ''
     loop do
         input = gets.chomp.downcase
@@ -28,10 +28,26 @@ scraper = ExchangeScraper.new
 scraper.make_connection
 
 if scraper.connected
-    puts display_exchange_table
+    table_data = scraper.supply
+    table_list = table_data.keys
+    exchange_data = table_data['country']
+
+    puts 'Success!'
+    puts ''
+    puts display_exchange_table(exchange_data)
+    puts''
+    loop do
+        puts 'Fetch Current Rate by Currency Symbol'
+        puts 'Example: <USD, JPY, GBP, ZAR>'
+        user_choice = get_user_data(curr_key)
+        break if user_choice.empty?
+
+        puts ''
+        country_data = table_data[user_choice]
+        puts display_exchange_table(country_data, user_choice)
+    end
 else
     puts ''
     puts "Connection Failed... Check your internet connection"
 end
 
-# 7. Display currency rate according to user input data
