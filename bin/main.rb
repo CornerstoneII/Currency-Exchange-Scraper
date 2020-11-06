54cb3a8f9ce7ed10a1cdf9b89aa72cc36d4acc9a
CornerstoneII <seunmyles@gmail.com>
2020-11-06 16:51:29 +0100

    Update user interface file

--- bin/main.rb
+++ bin/main.rb
@@ -1,17 +1,6 @@
 #!/usr/bin/env ruby
 require_relative '../lib/scraper'
-require_relative '../lib/utils'
 
-def get_user_data(check_curr_key)
-  input = ''
-  loop do
-  input = gets.chomp.downcase
-   break if check_curr_key.include?(input) || input.empty?
-
-  puts input + 'No Data Found'
-  end
-  input
-end
 
 puts '
 
@@ -29,9 +18,7 @@ gets
 puts '.....Fetching Exchange Rate'
 
 scraper = ExchangeScraper.new
-scraper.try_connecting
 
-if scraper.connection
   table_data = scraper.supply
   table_list = table_data.keys
   exchange_data = table_data['country']
@@ -40,17 +27,5 @@ if scraper.connection
   puts ''
   puts display_exchange_table(exchange_data)
   puts ''
-  loop do
-     puts 'Fetch Current Rate by Currency Symbol'
-     puts 'Example: <USD, JPY, GBP, ZAR>'
-     user_choice = get_user_data(curr_key)
-     break if user_choice.empty?
 
-     puts ''
-     country_data = table_data[user_choice]
-     puts display_exchange_table(country_data, user_choice)
-   end
-else
-  puts ''
-  puts 'Connection Failed... Check internet connection'
 end
