13208850a8c0289c7f536fb612de063f10efd2bc
CornerstoneII <seunmyles@gmail.com>
2020-11-10 00:00:11 +0100

    Add unit-testing as suggested in project review

---
+++ spec/store_spec.rb
@@ -0,0 +1,18 @@
+require './lib/scraper'
+
+describe ShopScraper do
+  describe '#display_names' do
+    it 'returns an array of product names' do
+      names = ShopScraper.new
+      expect(names.display_names).to be_an_instance_of(Array)
+    end
+  end
+
+  describe 'the #display_prices method' do
+    it 'returns an array of product prices' do
+      prices = ShopScraper.new
+      expect(prices.display_prices).to be_an_instance_of(Array)
+    end
+  end
+
+end
